6a600272b651d94a98994ba59196ff76484ec977
Muhammad Abdullah Sa'id <pvzuntil@gmail.com>
2024-11-15 08:37:54 +0700

    add: dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,16 +1,18 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.google.android.libraries.mapsplatform.secrets.gradle.plugin)
+    id("kotlin-parcelize")
 }
 
 android {
     namespace = "com.untillness.borwita"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.untillness.borwita"
         minSdk = 30
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
@@ -27,24 +29,49 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
     }
     kotlinOptions {
-        jvmTarget = "1.8"
+        jvmTarget = "11"
     }
     buildFeatures {
         viewBinding = true
+        buildConfig = true
     }
 }
 
 dependencies {
-
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+
+    // Pull to refresh
+    implementation(libs.androidx.swiperefreshlayout)
+
+    // Shimmer
+    implementation(libs.shimmer)
+
+    // Share preference
+    implementation(libs.androidx.preference)
+
+    // Retrofit
+    implementation(libs.logging.interceptor)
+    implementation(libs.converter.gson)
+    implementation(libs.okhttp)
+    implementation(libs.retrofit2.retrofit)
+
+    // Glide
+    implementation(libs.glide)
+
+    // Maps
+    implementation(libs.play.services.maps)
+
+    // Location
+    implementation(libs.play.services.location)
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
