21e42004f0e613528f4316616adf30b29ee6bac1
Muhammad Abdullah Sa'id <pvzuntil@gmail.com>
2024-11-16 06:08:23 +0700

    add: profile view

--- app/src/main/java/com/untillness/borwita/ui/wrapper/fragments/profile/ProfileFragment.kt
+++ app/src/main/java/com/untillness/borwita/ui/wrapper/fragments/profile/ProfileFragment.kt
@@ -7,11 +7,11 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import com.untillness.borwita.databinding.FragmentNotificationsBinding
+import com.untillness.borwita.databinding.FragmentProfileBinding
 
 class ProfileFragment : Fragment() {
 
-    private var _binding: FragmentNotificationsBinding? = null
+    private var _binding: FragmentProfileBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -25,13 +25,9 @@ class ProfileFragment : Fragment() {
         val profileViewModel =
             ViewModelProvider(this).get(ProfileViewModel::class.java)
 
-        _binding = FragmentNotificationsBinding.inflate(inflater, container, false)
+        _binding = FragmentProfileBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textNotifications
-        profileViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
         return root
     }
 
--- app/src/main/java/com/untillness/borwita/ui/wrapper/fragments/toko/TokoFragment.kt
+++ app/src/main/java/com/untillness/borwita/ui/wrapper/fragments/toko/TokoFragment.kt
@@ -7,11 +7,11 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import com.untillness.borwita.databinding.FragmentDashboardBinding
+import com.untillness.borwita.databinding.FragmentTokoBinding
 
 class TokoFragment : Fragment() {
 
-    private var _binding: FragmentDashboardBinding? = null
+    private var _binding: FragmentTokoBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -25,7 +25,7 @@ class TokoFragment : Fragment() {
         val tokoViewModel =
             ViewModelProvider(this).get(TokoViewModel::class.java)
 
-        _binding = FragmentDashboardBinding.inflate(inflater, container, false)
+        _binding = FragmentTokoBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
 //        val textView: TextView = binding.textDashboard
