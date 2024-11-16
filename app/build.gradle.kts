dd3b2caea060be59a3f8d7671d4ea3a22b138e74
Muhammad Abdullah Sa'id <pvzuntil@gmail.com>
2024-11-17 00:25:13 +0700

    add: camerax

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -76,6 +76,22 @@ dependencies {
     implementation(libs.androidx.navigation.fragment.ktx)
     implementation(libs.androidx.navigation.ui.ktx)
 
+    // Camerax
+    // CameraX core library using the camera2 implementation
+    // The following line is optional, as the core library is included indirectly by camera-camera2
+    implementation(libs.androidx.camera.core)
+    implementation(libs.androidx.camera.camera2)
+    // If you want to additionally use the CameraX Lifecycle library
+    implementation(libs.androidx.camera.lifecycle)
+    // If you want to additionally use the CameraX VideoCapture library
+    implementation(libs.androidx.camera.video)
+    // If you want to additionally use the CameraX View class
+    implementation(libs.androidx.camera.view)
+    // If you want to additionally add CameraX ML Kit Vision Integration
+    implementation(libs.androidx.camera.mlkit.vision)
+    // If you want to additionally use the CameraX Extensions library
+    implementation(libs.androidx.camera.extensions)
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
--- app/src/main/java/com/untillness/borwita/ui/toko_store/TokoStoreActivity.kt
+++ app/src/main/java/com/untillness/borwita/ui/toko_store/TokoStoreActivity.kt
@@ -1,5 +1,6 @@
 package com.untillness.borwita.ui.toko_store
 
+import android.content.Intent
 import android.os.Build
 import android.os.Bundle
 import android.view.WindowManager
@@ -8,12 +9,20 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.untillness.borwita.R
+import com.untillness.borwita.databinding.ActivityTokoStoreBinding
+import com.untillness.borwita.helpers.AppHelpers
 import com.untillness.borwita.helpers.Unfocus
+import com.untillness.borwita.ui.capture.CaptureActivity
 
 class TokoStoreActivity : Unfocus() {
+    private lateinit var binding: ActivityTokoStoreBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_toko_store)
+
+        this.binding = ActivityTokoStoreBinding.inflate(layoutInflater)
+
+        setContentView(this.binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
@@ -23,10 +32,21 @@ class TokoStoreActivity : Unfocus() {
         supportActionBar?.setDisplayHomeAsUpEnabled(true);
 
         title = "Tambah Toko"
+
+        this.triggers()
     }
 
     override fun onSupportNavigateUp(): Boolean {
         finish()
         return super.onSupportNavigateUp()
+    }
+
+    private fun triggers(){
+        this.binding.apply {
+            buttonKameraKtp.setOnClickListener {
+                val intent = Intent(this@TokoStoreActivity, CaptureActivity::class.java)
+                startActivity(intent)
+            }
+        }
     }
 }
