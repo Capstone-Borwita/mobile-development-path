7f62425b257522dc05accfb5ad8248d09031ece4
Muhammad Abdullah Sa'id <pvzuntil@gmail.com>
2024-11-24 16:45:51 +0700

    add: api detail toko

--- app/src/main/java/com/untillness/borwita/data/remote/api/ApiInterface.kt
+++ app/src/main/java/com/untillness/borwita/data/remote/api/ApiInterface.kt
@@ -8,6 +8,7 @@ import com.untillness.borwita.data.remote.responses.LoginResponse
 import com.untillness.borwita.data.remote.responses.OcrResponse
 import com.untillness.borwita.data.remote.responses.ProfileResponse
 import com.untillness.borwita.data.remote.responses.RegisterResponse
+import com.untillness.borwita.data.remote.responses.TokoDetailResponse
 import com.untillness.borwita.data.remote.responses.TokoResponse
 import okhttp3.MultipartBody
 import okhttp3.RequestBody
@@ -18,6 +19,7 @@ import retrofit2.http.Multipart
 import retrofit2.http.POST
 import retrofit2.http.PUT
 import retrofit2.http.Part
+import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface ApiInterface {
@@ -92,4 +94,9 @@ interface ApiInterface {
 
     @GET("/api/v1/stores/")
     suspend fun listToko(): Response<TokoResponse>
+
+    @GET("/api/v1/stores/{id}")
+    suspend fun detailToko(
+        @Path("id") id: String,
+    ): Response<TokoDetailResponse>
 }
